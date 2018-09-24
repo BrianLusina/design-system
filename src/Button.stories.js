4f2379bfe4f1a8a07317273984720f367a8e5be1
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2018-09-24 06:45:29 +0300

    New Dependency => withInfo, used to display more information of a component

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -2,11 +2,31 @@ import React from 'react'
 import {
     storiesOf
 } from "@storybook/react";
+import { withInfo } from "@storybook/addon-info"
 import {
     Button
 } from './Button';
 
 
-storiesOf("Button", module).add("with red background",() => {
-    return <Button bg="red">Hello World</Button>
-})
+storiesOf("Button", module)
+.addWithJSX(
+    "with red background", 
+    withInfo({
+        styles: {
+            header: {
+                h1: {
+                    color: 'red'
+                }
+            }
+        },
+        text: `
+            Description of Button
+
+            ~~~js
+            <Button>Hello World</Button>
+            ~~~
+        `
+    })(() => {
+        return <Button bg="red">Hello World</Button>
+    })
+)
